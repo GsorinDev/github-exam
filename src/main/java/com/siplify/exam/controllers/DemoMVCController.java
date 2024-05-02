caff916a3bca8a6a78db3cd182d7341b71e07c49
Georgio Sorin <georgio.sorin@gmail.com>
2024-05-02 16:58:40 +0200

    spring

--- src/main/java/com/siplify/exam/controllers/DemoMVCController.java
+++ src/main/java/com/siplify/exam/controllers/DemoMVCController.java
@@ -1,6 +1,7 @@
 package com.siplify.exam.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoMVCController {
@@ -8,5 +9,10 @@ public class DemoMVCController {
     public String displayMessage()
     {
         return "Hello Ynov";
+    }
+
+    @GetMapping("/ynov")
+    public String displayMessageHTML() {
+        return "ynov/ynov";
     }
 }
--- src/test/java/com/siplify/exam/TestDemoMVCController.java
+++ src/test/java/com/siplify/exam/TestDemoMVCController.java
@@ -1,20 +1,37 @@
 package com.siplify.exam;
 
 import com.siplify.exam.controllers.DemoMVCController;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @WebMvcTest
 public class TestDemoMVCController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
     @Test
     public void TestDisplayMessage() throws Exception
     {
         DemoMVCController controller = new DemoMVCController();
 
         assertEquals("Hello Ynov", controller.displayMessage());
+    }
+
+    @Test
+    public void Test_info() throws Exception
+    {
+        this.mockMvc.perform(get("/ynov")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Ynov")));
     }
 }
